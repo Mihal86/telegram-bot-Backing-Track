4f02cc7c7458258a42f9af831d48662c9800aec0
Mihal86 <103967539+Mihal86@users.noreply.github.com>
2025-04-05 22:03:31 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,13 @@
-print("Бот працює") 
+import os
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
+TOKEN = os.getenv("BOT_TOKEN")  # Отримуємо токен з Railway
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("Привіт! Я твій бот!")
+
+updater = Updater(TOKEN)
+updater.dispatcher.add_handler(CommandHandler("start", start))
+updater.start_polling()
+updater.idle()
