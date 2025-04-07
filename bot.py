3df38aa945994d5b4bd65a726f55bc7d85e8a692
Mihal86 <103967539+Mihal86@users.noreply.github.com>
2025-04-07 21:35:48 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -2,23 +2,28 @@ import os
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
-from aiogram.utils import executor
+from aiogram import F
+import asyncio
 
 # Завантажуємо токен бота з змінних середовища
 TOKEN = os.getenv("RAILWAY_TOKEN")
 
 # Ініціалізуємо бота та диспетчер
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
 # Налаштовуємо логування
 logging.basicConfig(level=logging.INFO)
 
 # Обробник команди /start
-@dp.message_handler(commands=['start'])
+@dp.message(commands=['start'])
 async def send_welcome(message: Message):
-    await message.reply("Привіт, я твій бот!")
+    await message.answer("Привіт, я твій бот!")
 
-# Запуск бота
+async def main():
+    dp.include_router(dp)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
