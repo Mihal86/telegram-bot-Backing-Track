4daaf7b8a684722f33eb7c71098a71167df27360
Mihal86 <103967539+Mihal86@users.noreply.github.com>
2025-04-09 23:03:58 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,25 @@
 import os
 import psycopg2
 
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import Application, CommandHandler, CallbackContext
+
+# Отримуємо URI з середовища
 DATABASE_URL = os.getenv("DATABASE_PUBLIC_URL")
 
+# Підключення до PostgreSQL
 def get_db_connection():
+    conn = psycopg2.connect(DATABASE_URL)
+    return conn
+
+# Перевірка з'єднання
+def test_db_connection():
     try:
-        conn = psycopg2.connect(DATABASE_URL)
-        return conn
+        conn = get_db_connection()
+        print("Підключення до бази даних успішне!")
+        conn.close()
     except Exception as e:
         print(f"Помилка при підключенні до бази даних: {e}")
-        return None
+
+# Викликаємо тестову функцію, щоб перевірити підключення
+test_db_connection()
