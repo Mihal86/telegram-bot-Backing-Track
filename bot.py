e563cfdf48abb37958247794073620479345f824
Mihal86 <103967539+Mihal86@users.noreply.github.com>
2025-04-15 23:30:02 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from db import test_db_connection
 
 load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+RAILWAY_TOKEN = os.getenv("RAILWAY_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привіт! Бот працює ✅")
@@ -14,6 +14,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 if __name__ == "__main__":
     asyncio.run(test_db_connection())  # тест БД при старті
 
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app = ApplicationBuilder().token(RAILWAY_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
